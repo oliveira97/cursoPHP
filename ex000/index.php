87df7f90f474b0de5af66e5c420095d06634ac87
Thiago Rodrigues Oliveira <thiagordrgs89@gmail.com>
2023-06-11 20:11:04 -0300

    Update index.php

--- ex000/index.php
+++ ex000/index.php
@@ -4,10 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Me livrando da maldição</title>
+    <style>
+        p {
+            font-size: 20px;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
 </head>
 <body>
     <h1>
-
+        <?php 
+            echo "Hello, word! \u{1F418} \u{1F30E}";
+        ?>
     </h1>
     <p>Vamos tentar nos livrar da maldição!</p>
 </body>
