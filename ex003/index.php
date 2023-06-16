b47f751dfc177722e113c065a9cad6c162a3cea4
Thiago Rodrigues Oliveira <thiagordrgs89@gmail.com>
2023-06-15 21:01:19 -0300

    Create index.php

---
+++ ex003/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Testes de tipos primitivos</h1>
+
+    <?php 
+        // $num = 0x1A;
+        // echo "O valor da variável é $num";
+        
+        // $v = 300;
+        // var_dump($v);
+
+        // $num = 3e2;
+        // echo "O valor é $num";
+
+    
+    ?>
+</body>
+</html>
