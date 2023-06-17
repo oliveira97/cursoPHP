404dcd3a27a60ae3e56b27bb06034290d30b2994
Thiago Rodrigues Oliveira <thiagordrgs89@gmail.com>
2023-06-17 09:59:43 -0300

    Update teste.php

---
+++ testes/teste.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste 01</title>
+</head>
+<body>
+    <h1>Mango loco</h1>
+
+    <?php 
+        $nome = "Thiago";
+        echo "Olá $nome";
+        echo 'Olá $nome';
+
+        
+    
+    ?>
+</body>
+</html>
