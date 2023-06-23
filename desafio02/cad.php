8a92642a7228c79438a78fa60eae29383c563a74
Thiago Rodrigues Oliveira <thiagordrgs89@gmail.com>
2023-06-23 10:22:36 -0300

    att desafio 02

---
+++ desafio02/cad.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sorteador</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <?php
+            $numero = mt_rand(1, 100);
+            echo $numero;
+        ?>
+    <button onclick="javascript:window.location.href='index.html'">&#x2B05; Voltar</button>    
+    </main>
+</body>
+</html>
